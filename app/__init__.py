904f10e72e50393781ab22762acbaf8d80562cde
Angel Cancio <4ng31@users.noreply.github.com>
2019-01-24 15:27:53 -0300

    Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+# def create_app():
+#   app = Flask(__name__)
+#   Bootstrap(app)
+#
+#   return app
 
 @app.route("/")
+def main():
+    return render_template('index.html')
+    #return "Probando flask!"
 
-def hello():
-    return "Probando flask!"
+
+@app.route("/acm")
+def acm():
+    return "Hello, Angel"
 
 
 if __name__ == "__main__":
